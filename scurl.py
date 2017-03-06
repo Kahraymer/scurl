781289aa67a5d47d1dcf9b21f63bfbe0d20ad896
Kahraymer <tomadamkremer@gmail.com>
2017-03-05 18:01:39 -0800

    Added main file

---
+++ scurl.py
@@ -0,0 +1,13 @@
+# Tom Kremer
+# Ben Krausz
+
+from sys import argv, stdout
+from socket import socket
+
+from OpenSSL.SSL import TLSv1_METHOD, Context, Connection
+
+
+def main():
+
+if __name__ == __main__:
+	main()
