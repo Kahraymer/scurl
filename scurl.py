2b517696ad96b434df3a8fb008a54065d716d204
Kahraymer <tomadamkremer@gmail.com>
2017-03-07 20:43:29 -0800

    Commented

--- scurl.py
+++ scurl.py
@@ -8,20 +8,25 @@ from OpenSSL import SSL
 from OpenSSL import crypto
 
 def main():
+	# Setting up socket, context, and connection
 	sock = socket()
 	mycontext = SSL.Context(SSL.TLSv1_METHOD)
 	myconn = SSL.Connection(mycontext, sock)
 	
+	# Connecting to a website and exchanging keys
 	myconn.connect(('www.facebook.com', 443))
 	myconn.do_handshake()
 
 	print "Connection established"
 
+	# Sending a dummy message to poke website and get error message page
+	# Should be sending some sort of GET request
 	print myconn.getpeername()
 	print myconn.state_string()
 	print myconn.sendall("testing")
 	print "Sent that ish"
 
+	# Receiving Facebook's error page html
 	t1 = []
 	try:
 		while True:
